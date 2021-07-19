43368c2bb2bd2b3627bd32037e32170bf4288781
Oli Evans <oli@tableflip.io>
2021-07-20 00:17:57 +0100

    feat: tests!
    License: (Apache-2.0 AND MIT)
    Signed-off-by: Oli Evans <oli@tableflip.io>

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-const { filesFromPath } = require('files-from-path')
-const { Web3Storage } = require('web3.storage')
 const core = require('@actions/core')
+const { addToWeb3, pickName } = require('./web3')
+
+const {
+  GITHUB_REPOSITORY,
+  GITHUB_SHA
+} = process.env
 
 async function run () {
   try {
-    const pathToAdd = core.getInput('path_to_add')
     const endpoint = new URL(core.getInput('web3_api'))
     const token = core.getInput('web3_token')
-    const web3 = new Web3Storage({ endpoint, token })
-
+    const pathToAdd = core.getInput('path_to_add')
     core.info(`Adding ${pathToAdd} to ${endpoint.origin}`)
-    const name = `${GITHUB_REPOSITORY.replace('/', '-')}-${GITHUB_SHA.substring(0,8)}`
-    const cid = await web3.put(filesFromPath(`${pathToAdd}/**`), { name })
-    const url = `https://dweb.link/ipfs/${cid}`
+    const name = pickName(GITHUB_REPOSITORY, GITHUB_SHA)
+    const { cid, url } = await addToWeb3({endpoint, token, name, pathToAdd})
     core.info(url)
     core.setOutput('cid', cid)
-    core.setOutput('url', cid)
+    core.setOutput('url', url)
   } catch (error) {
     core.setFailed(error.message)
   }
